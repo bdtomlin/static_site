e84b237a3fdad52242acede246e8ab6528cb39d4
Bryan Tomlin <bryan@tomlin.email>
2024-09-02 11:46:29 -0500

    block to block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,6 +1,68 @@
+import re
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     blocks = map(lambda b: b.strip(), blocks)
     blocks = filter(lambda b: b != "", blocks)
     blocks = list(blocks)
     return blocks
+
+
+def block_to_block_type(block):
+    if is_heading(block):
+        return heading_type(block)
+    elif is_code(block):
+        return "code"
+    elif is_quote(block):
+        return "quote"
+    elif is_unordered_list(block):
+        return "ul"
+    elif is_ordered_list(block):
+        return "ol"
+    else:
+        return "p"
+
+
+def is_heading(block):
+    return len(re.findall(r"^#{1,6}\s", block))
+
+
+def heading_type(block):
+    hashes = block.split(" ", 1)[0]
+    level = len(hashes)
+    return f"h{level}"
+
+
+def is_code(block):
+    return block.startswith("```") and block.endswith("```")
+
+
+def is_quote(block):
+    lines = block.split("\n")
+    filtered = filter(lambda line: line.startswith("> "), lines)
+    return len(lines) == len(list(filtered))
+
+
+def is_unordered_list(block):
+    lines = block.split("\n")
+
+    def filter_func(line):
+        return line.startswith("* ") or line.startswith("- ")
+
+    filtered = filter(filter_func, lines)
+    return len(lines) == len(list(filtered))
+
+
+def is_ordered_list(block):
+    lines = block.split("\n")
+
+    for i in range(len(lines)):
+        if lines[i].startswith(f"{i+1}. "):
+            continue
+        return False
+
+    return True
+
+    filtered = filter(filter_func, lines)
+    return len(lines) == len(list(filtered))
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdownBlocks(unittest.TestCase):
@@ -29,3 +29,60 @@ class TestMarkdownBlocks(unittest.TestCase):
         expected = []
         actual = markdown_to_blocks(markdown)
         self.assertEqual(expected, actual)
+
+    def test_block_to_block_type__heading(self):
+        for i in range(6):
+            n = i + 1
+            hshs = "#" * n
+            block = f"{hshs} Some Text"
+            expected = f"h{n}"
+            actual = block_to_block_type(block)
+            self.assertEqual(expected, actual)
+
+    def test_block_to_block_type__code(self):
+        block = "```here's the code```"
+        expected = "code"
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type__quote(self):
+        block = "> this\n> is\n> a quote"
+        expected = "quote"
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type__not_quote(self):
+        block = "> this\n is not\n> a quote"
+        not_expected = "quote"
+        actual = block_to_block_type(block)
+        self.assertNotEqual(not_expected, actual)
+
+    def test_block_to_block_type__ul(self):
+        block = "* this\n* is\n* a ul"
+        expected = "ul"
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type__not_ul(self):
+        block = "* this\n is not\n* a ul"
+        not_expected = "quote"
+        actual = block_to_block_type(block)
+        self.assertNotEqual(not_expected, actual)
+
+    def test_block_to_block_type__ol(self):
+        block = "1. this\n2. is\n3. a ol"
+        expected = "ol"
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type__not_ol(self):
+        block = "1. this\n is not\n3. a ol"
+        not_expected = "ol"
+        actual = block_to_block_type(block)
+        self.assertNotEqual(not_expected, actual)
+
+    def test_block_to_block_type__not_ol2(self):
+        block = "1. this\n3. is not\n2. a ol"
+        not_expected = "ol"
+        actual = block_to_block_type(block)
+        self.assertNotEqual(not_expected, actual)
